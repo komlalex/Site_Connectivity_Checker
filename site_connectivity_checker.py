8199c93fbaca2d814729c6e80e56ae7855dfdd83
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 08:13:44 +0000

    Final commit

--- site_connectivity_checker.py
+++ site_connectivity_checker.py
@@ -3,6 +3,18 @@
 # user urllib.request to get data from the url
 # write a function that takes a url and returns a response
 """
+from urllib import request
 
 
 
+def main(url): 
+    print('Checking connectivity status...')
+    response = request.urlopen(url)
+    print(f"Connected to {url} successfully")
+    print(f"The response code was: {response.code}")
+
+
+
+print("This is a site connectivity checker. ")
+input_url = input("Input the url of the site you want to check:  ")
+main(input_url)
